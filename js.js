c3df80dab31e29d1d133afd9b53d6d103b784a4b
Allison O'Hanlon <allison.t.ohanlon@gmail.com>
2017-05-01 22:21:03 -0500

    monday night

--- js.js
+++ js.js
@@ -15,12 +15,12 @@ $("#vadar").append("<p>Health Points: " + vadarPoints +
 $("#hutt").append("<p>Health Points: " + huttPoints + 
 	"</p>");	
 
-
 $('.individual').click(function selectCharacter(event1) {
 	if (clicks === 0) {
   		$(this).prepend("<h3>Selected Player</h3>");
 		$(this).css({"background-color": "green"})
 		$(".game").append(this);
+		$(this).attr("id", "selectCharacter");
 
 		$('.individual').not(this).prepend("<h3>Enemy</h3>");
 		$('.individual').not(this).css({"background-color": "red"});
@@ -43,16 +43,33 @@ $('.individual').click(function selectCharacter(event1) {
 
 //subtract points when the attack button is pushed on the selectCharacter and selectEnemy 
 //use multiple to make one of the characers multiply every time that the attack button is pushed
-if (chewbaccaPoints > 0) {
-$('#attack').click(function minusPoints() {
-	var chewbaccaPoints = chewbaccaPoints - 10;
-	$("#chewbaccaPoints").html("<p id='chewbaccaPoints'>Health Points: " + chewbaccaPoints + 
-	"</p>");
-	});
-}
-else if (chewbaccaPoints = 0) {
-	$("#chewbaccaPoints").html("<p id='chewbaccaPoints'>DEAD</p>");
-}
+// if (chewbaccaPoints > 0) {
+
+$('#attack').click(function () {
+		if (chewbaccaPoints > 0) {
+		chewbaccaPoints = chewbaccaPoints - 10;
+		$("#soloPoints").html("<p id='soloPoints'>Health Points: " + chewbaccaPoints + 
+		"</p>");
+		}
+		else {
+		$("#chewbaccaPoints").html("<p id='chewbaccaPoints'>DEAD</p>");
+		}
+});		
+
+	// else if (character = $("#han-solo")) {
+	// 	if (soloPoints > 0) {
+	// 	soloPoints = soloPoints - 10;
+	// 	$("#soloPoints").html("<p id='soloPoints'>Health Points: " + soloPoints + 
+	// 	"</p>");
+	// 	else {
+	// 	$("#soloPoints").html("<p id='soloPoints'>DEAD</p>");
+	// 	}	
+	
+
+// }
+// else if (chewbaccaPoints = 0) {
+// 	$("#chewbaccaPoints").html("<p id='chewbaccaPoints'>DEAD</p>");
+// }
 
 //make it so it says "you're out of tries refresh"
 //fade out an "enemy" when you defeat them
